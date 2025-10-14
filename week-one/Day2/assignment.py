70f0e42f922afc5eae45aec81f75817562356ec5
James Kabuga <jamesndungukabuga@gmail.com>
2025-10-14 15:30:41 +0300

    Day2: Learnt Variables and Datatypes

---
+++ week-one/Day2/assignment.py
@@ -0,0 +1,15 @@
+# Write a python script that will ask the user to enter their name , age and favourite number
+# Print a message using these values
+# Convert favourite number to float and print its type
+
+# Getting user input
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+favourite_number = input("Enter your favourite number:")
+
+print("Hello" , name  , "you are" , age , "years old and your favourite number is" , favourite_number)
+
+
+
+print("The type of your Favourite color is:" , type(favourite_number))
