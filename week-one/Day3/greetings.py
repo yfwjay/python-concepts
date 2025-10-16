f7951d1323e5557e55c6ca9404a214bae81c2204
James Kabuga <jamesndungukabuga@gmail.com>
2025-10-16 23:15:48 +0300

    Day3:Learnt Indexing and Slicing

---
+++ week-one/Day3/greetings.py
@@ -0,0 +1,13 @@
+greetings = "Hello, World!"
+
+# Indexing
+print(greetings[0]) # Output: H
+print(greetings[-1]) # Output: !
+
+# Slicing
+print(greetings[7:12]) # Output: World
+print(greetings[:5]) # Output: Hello
+
+
+
+
