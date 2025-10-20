7e9da6926211b808949402b1286bd8ffcd0d64cf
James Kabuga <jamesndungukabuga@gmail.com>
2025-10-20 14:35:42 +0300

    Day2:Learnt abour Decision Making using if-else statements

---
+++ week-two/Day2/if_else.py
@@ -0,0 +1,13 @@
+# I'll generate a simple grading system
+
+grade = float(input("Enter your Marks = "))
+
+if grade >= 70 :
+    print("Your grade is = A ")
+elif grade >= 60 :
+    print("Your grade is = B")
+elif grade >= 40 :
+    print("Your grade is = C")
+else:
+    print("Your grade is  = D")
+
