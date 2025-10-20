048e8ca274101dc535802905c4fdae04a0b52bb4
James Kabuga <jamesndungukabuga@gmail.com>
2025-10-20 13:24:25 +0300

    Day1:Learnt about Operators and Expressions in Python and did an assignment on the same

---
+++ week-two/Day1/assignment.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter any number = "))
+num2 = float(input("Enter the second number = "))
+
+addition = num1 + num2
+difference = num1 - num2
+same = num1 == num2
+
+print("Addition = " ,  addition)
+print("Subtraction = " , difference)
+print("Is num1 and num2 equal = " , same)
+
+# Reassigning
+
+addition += 10
+
+print("New summation after reassigning by 10 = " , addition)
---
+++ week-two/Day1/operators.py
@@ -0,0 +1,40 @@
+# Expressions
+# Any piece of code that returns a value.
+
+num1 = 10
+num2 = 2
+
+# Operators
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+modulus = num1 % num2
+power = num1 ** num2
+flow_division = num1 // num2
+
+# Comparison Operators
+
+print(num1 == num2)
+print(num1 < num2)
+print(num1 > num2)
+
+# Logical operators
+
+print(num1 < num2 and num1 == num2)
+print(num1 < num2 or num1 > num2)
+print(not(num1 > num2))
+
+# Reassigning using assignment operators
+
+# We increase the value of addition by 4
+addition += 4  
+print(addition)
+
+# We decrease the value of addition by 4
+addition -= 4
+
+print(addition)
+
+
