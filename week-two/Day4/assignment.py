9862269bab320bfd11c2fb6605f80c7ef7084802
James Kabuga <jamesndungukabuga@gmail.com>
2025-10-24 23:07:29 +0300

    Day4: Learnt about For loops

---
+++ week-two/Day4/assignment.py
@@ -0,0 +1,5 @@
+# A for loop that prints even numbers in range 1 and 100
+
+for num in range(1 , 100):
+    if num % 2 == 0:
+        print(f"This is an even number : {num}")
---
+++ week-two/Day4/for-loop.py
@@ -0,0 +1,14 @@
+# For loops
+# For every iteration(i) we do a certain action. And the iteration are 10 times.
+
+
+# for i in range(10):
+#     print(f"This is number: {i}")
+
+
+# We try implimenting the list one.
+
+names = ['James' , 'jake' , 'joy' , 'kane']
+
+for name in names:
+        print(f"This is my name : {name}")
