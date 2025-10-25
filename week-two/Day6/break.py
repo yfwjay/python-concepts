003b1a9f446e8d3367582ad17632494512bcc349
James Kabuga <jamesndungukabuga@gmail.com>
2025-10-25 23:46:45 +0300

    Day6:Learnt how to impliment break and continue in loops

---
+++ week-two/Day6/break.py
@@ -0,0 +1,14 @@
+# Break keyword
+
+for i in range(10):
+    if i == 5:
+        break
+    print(f"This is a number {i}")
+
+
+num = 0
+
+while num != 7:
+    print(f"This is number: {num}")
+    num += 1
+    
---
+++ week-two/Day6/continue.py
@@ -0,0 +1,6 @@
+# It skips iteration number 7
+
+for i in range(10):
+    if i == 7:
+        continue
+    print(f"This is number {i}")
