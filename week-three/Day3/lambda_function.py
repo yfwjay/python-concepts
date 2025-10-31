44b76790e2bbe4bb288a423775cc107df18f9959
James Kabuga <jamesndungukabuga@gmail.com>
2025-10-31 23:03:33 +0300

    Day3:Learnt about return statements and lambda functions.

---
+++ week-three/Day3/lambda_function.py
@@ -0,0 +1,9 @@
+# This lambda function is intended to add two numbers.
+add = lambda x , y : x + y
+
+print(add(3, 5))  # Output: 8
+
+#  A lambda function to calculate the square of a number
+square = lambda x : x ** 2
+
+print(square(4))  # Output: 16
