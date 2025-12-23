aecb5ed715953032fbaf96e87b85002715ca3905
yfwjay <jamesndungukabuga@gmail.com>
2025-12-23 18:09:47 +0300

    Did a recap on Sets

---
+++ iterations/Sets/details.py
@@ -0,0 +1,12 @@
+# SETS defined using curly braces
+
+numbers = {0 , 1, 2 ,3, 4 ,5, 2, 4, 7,8,9,10}
+
+# print the type of our set to confirm its of class set
+
+print(type(numbers))
+
+# Now print the items in our set
+
+print(numbers) # notice it eliminates all duplicates and sorts them in ascending order
+
